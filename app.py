abbcdacd685467c91a8c52c655e5c233a303fc50
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-03-20 12:43:50 +1300

    ..

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ from mongoengine import *
 app = Flask(__name__)
 connect('mongo_knuddy')
 
-class User(Document):
-	email = StringField()
-	first_name = StringField()
-	last_name = StringField()
+class Country(Document):
+	country_name = StringField()
 
 
 @app.route('/')
@@ -21,21 +19,28 @@ def index():
 	return render_template("index.html", **kwargs)
 
 
-@app.route('/sacred')
-def upload():
-	user = User(email="FAk3@gmail.com", first_name="Dean", last_name="Knudson")
-	user.save()
-
-	for u in User.objects:
-		print("{} {} : {}".format(u["first_name"], u["last_name"], u["email"]))
-	
-	return redirect(url_for('index'))	
-
-
 @app.route('/inspiration')
 def inspiration():
 	page_title = "Inspirations"
 	return render_template("inspirations.html", page_title=page_title)
+
+
+@app.route('/create/<string:country_name>', methods=['POST'])
+def create_country(country_name):
+	country = Country(country_name=country_name)
+	country.save()
+	redirect(url_for(get_country(country_id=country.id)))
+
+
+@app.route('/country', methods=['GET'])
+@app.route('/country/<int:country_id>', methods=['GET'])
+def get_country(country_id=None):
+	if country_id is None:
+		countries = Country.objects
+	else:
+		countries = Country.objects.get(id=country_id)
+	return countries.to_json()
+
 
 if __name__ =="__main__":
 	app.run(debug=True, host='0.0.0.0', port=80)
