db3318ff18e9fe9c9d5e9ecc77760e4f551ee680
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-04-05 11:08:21 +1300

    ..

--- static/js/visualisation.js
+++ static/js/visualisation.js
@@ -34,3 +34,9 @@ function update(data){
     c.attr("r",10)
     .attr("transform",function(d,i){ return "translate(" +  getRandomInt(1, width) + "," +  getRandomInt(1, height) + ")"})
 }
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
