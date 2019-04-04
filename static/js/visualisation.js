fa79f459a8cbb7bb8d5c8bc7eca5bbbdbe016ed3
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-04-05 12:04:22 +1300

    ..

--- static/js/visualisation.js
+++ static/js/visualisation.js
@@ -1,23 +1,13 @@
 function displayData(URL){
     $.get(URL, function(response){
       var responseObj = JSON.parse(response);
-      var prepared = prepare_data(responseObj[0].cell_data);
-      update(prepared);
-      update(prepared);
-      // update(prepared);
+      update(responseObj);
+      update(responseObj);
     }).fail(function(){
         console.log("ERROR");
     }).always(function(){
         console.log("DONE!");
     });
-}
-
-function prepare_data(data){
-    var out = []
-    for(var i = 0; i < data.length; i++){
-        out[i] = data[i].payload
-    }
-    return out
 }
 
 function update(data){
@@ -37,11 +27,23 @@ function update(data){
     c.enter().append("circle");
 
     c.attr("r",10)
-    .attr("transform",function(d,i){ return "translate(" +  scale(d) + "," +  scale(d) + ")"})
+    .attr("transform",function(d, i){ return "translate(" +  getRandomInt(1, width) + "," + getRandomInt(1, width) + ")"})
+    .attr("fill", getRandomColor());
+
+    c.append("text").text(function(d){ return d.year})
 }
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
