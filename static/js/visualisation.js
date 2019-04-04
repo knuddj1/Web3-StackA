7042963ddbb43435312482d9f7e8dbd5b2a9b300
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-04-05 10:49:26 +1300

    ..

--- static/js/visualisation.js
+++ static/js/visualisation.js
@@ -1,24 +1,31 @@
-// var data = [1,2,3,4,5]
-// var canvas = d3.select("#visualisation")
-
-// var circle = svg.selectAll("circle")
-//   .data(data);
-
-// circle.exit().remove();
-
-// circle.enter().append("circle")
-//     .attr("r", 2.5)
-//   .merge(circle)
-//     .attr("cx", function(d) { return d.x; })
-//     .attr("cy", function(d) { return d.y; });
-
-
 function displayData(URL){
     $.get(URL, function(response){
-        console.log(response)
+      var responseObj = JSON.parse(response);
+      console.log(response);
+      console.log(responseObj);
     }).fail(function(){
         console.log("ERROR");
     }).always(function(){
         console.log("DONE!");
     });
+}
+
+function prepare_data(data){
+
+}
+
+function update(data){
+    var bBox = d3.select("svg");
+    width = +bBox.attr("width");
+    height = +bBox.attr("height");
+    data = []
+    for(var i = 0; i < 20; i ++){
+        data[i] = getRandomInt(1, width)
+    }
+    var c = bBox.selectAll("circle")
+    .data(data);
+    c.exit().remove();
+    c.enter().append("circle");
+    c.attr("r",10)
+    .attr("transform",function(d,i){ return "translate(" +  getRandomInt(1, width) + "," +  getRandomInt(1, height) + ")"})
 }
